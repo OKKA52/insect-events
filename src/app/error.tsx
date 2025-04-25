403cf72fcb7c664e13a7be3b2ec269714d87fdca
OKKA52 <okadataketo@hotmail.com>
2025-04-25 18:55:48 +0900

    fix: recreate error.tsx without problematic icon

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,8 @@
 export default function ErrorPage() {
   return (
-    <section className="bg-white">
-      <div className="layout flex min-h-screen flex-col items-center justify-center text-center text-black">
-        <h1 className="mt-8 text-4xl">Something went wrong!</h1>
+    <section className='bg-white'>
+      <div className='layout flex min-h-screen  flex-col items-center justify-center text-center text-black'>
+        <h1 className='mt-8 text-4xl'>Something went wrong!</h1>
       </div>
     </section>
   );
