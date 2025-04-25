09e0580cc650ba45196fc44d904c628a161a3025
OKKA52 <okadataketo@hotmail.com>
2025-04-25 19:01:06 +0900

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,11 @@
 export default function ErrorPage() {
   return (
-    <section className='bg-white'>
-      <div className='layout flex min-h-screen  flex-col items-center justify-center text-center text-black'>
-        <h1 className='mt-8 text-4xl'>Something went wrong!</h1>
+    <section className="bg-white">
+      <div className="layout flex min-h-screen flex-col items-center justify-center text-center text-black">
+        <h1 className="text-4xl font-bold">エラーが発生しました</h1>
+        <p className="mt-4">しばらくしてから再度お試しください。</p>
       </div>
     </section>
   );
 }
+
