105a7b210ef9ce3e79b751915dedee43601dd58a
OKKA52 <okadataketo@hotmail.com>
2025-04-30 07:59:38 +0900

    feat: スマホボタン修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -204,7 +204,7 @@ export default function HomePage() {
             <button
               //onClick={handleClear}
               onClick={handleClear}
-              className='ml-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 text-sm'
+              className='p-2 border rounded bg-blue-500 text-white rounded hover:bg-blue-600 text-sm'
             >
               クリア
             </button>
