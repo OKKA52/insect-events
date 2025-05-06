e60432dbb175107e759fd55473c3467fe6a29dc7
OKKA52 <okadataketo@hotmail.com>
2025-05-07 06:12:07 +0900

    chore: remove unused __tests__ directory

--- src/__mocks__/svg.tsx
+++ src/__mocks__/svg.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-// Mock用のSVGコンポーネントを作成
+//  Mock用のSVGコンポーネントを作成
 const MockSvg = () => (
   <svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
     <circle cx='50' cy='50' r='40' />
